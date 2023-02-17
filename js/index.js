66763cdf9722f028ef7e10ea8f2f7a156ec0343e
luciaguerra <luciaguerra441@gmail.com>
2023-02-16 23:54:21 -0300

    v1.0.3

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-
-let cards = [firstCard, secondCard];
-
-let sum = firstCard + secondCard;
-
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 
 let message = " ";
 let messageEl = document.getElementById("message-el");
@@ -14,10 +9,25 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function getRandomCard() {
-    return 5;
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+
+    if (randomNumber >= 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
 }
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    
+    cards =  [firstCard, secondCard];
+    sum = firstCard + secondCard;
+
     renderGame();
 }
 
@@ -44,10 +54,48 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card);
-    console.log(cards);
-    renderGame();
+    if (isAlive === true && hasBlackjack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }     
 }
+
+/*let hasCompletedCourse = false;
+let givesCertificate = false;
+
+if (hasCompletedCourse === true && givesCertificate === true) {
+    generateCertificate();
+} else {
+    console.log("Try again next time!");
+}
+
+function generateCertificate() {
+    console.log("Generating certificate...");
+}
+
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+
+if (hasSolvedChallenge === false && hasHintsLeft === false){
+    showSolution();
+}
+
+
+function showSolution() {
+    console.log("Showing the solution...")
+}
+
+let likesDocumentaries = true;
+let likesStartups = false;
+
+if (likesDocumentaries === true || likesStartups === true) {
+    recommendMovie();
+}
+
+function recommendMovie() {
+    console.log("Hey, check out this new film we think you will like!")
+}*/
+
 
